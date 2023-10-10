afe9a98f157384392f6dccf84e29de9d933b9e38
Celse13 <rugiracelse13@gmail.com>
2023-10-10 08:18:57 -0700

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Implement the templated for the base class."""
+import datetime
+import uuid4
+
+class BaseModels:
+    """Template for  all items for the HBNB Console."""
+        def __init__(self, *args, **kwargs):
+            """Instintiated the base models class.
+            Args:
+                *args: additional arguments provided to the program
+                **kwargs: Additional key value to the program.
+            """
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        pass
