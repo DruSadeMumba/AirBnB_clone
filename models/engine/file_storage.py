babe8e26061874fb6ef5961be1c6be9da8076c8d
Dru Mumba <drusademumba@gmail.com>
2023-10-11 09:32:02 +0300

    Add method that returns the dictionary __objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""FileStorage Class"""
+
+
+class FileStorage:
+    """A class that serializes and deserializes instances to and fro JSON file"""
+    __file_path = "file_storage.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """sets in the obj with key"""
+        pass
+
+    def save(self):
+        """serializes __objects to the JSON file"""
+        pass
+
+    def reload(self):
+        """Deserializes the JSON file to __objects"""
+        pass
