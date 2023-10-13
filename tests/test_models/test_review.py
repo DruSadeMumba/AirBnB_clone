14bb67eb3477bc26a518777229d3df54a13e5ce4
Dru Mumba <drusademumba@gmail.com>
2023-10-13 17:16:27 +0300

    Add Review class tests

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,13 +1,44 @@
+#!/usr/bin/python3
+"""Unittest module for the Review Class."""
+import datetime
 import unittest
+from models.review import Review
+from models.base_model import BaseModel
 
 
 class TestReview(unittest.TestCase):
+    new = Review()
+    attr_types = {
+        'id': str,
+        'created_at': datetime.datetime,
+        'updated_at': datetime.datetime,
+        'place_id': str,
+        'user_id': str,
+        'text': str
+    }
 
     def setUp(self) -> None:
         super().setUp()
 
     def tearDown(self) -> None:
         super().tearDown()
+
+    def assertAttributes(self, instance):
+        """attributes data types"""
+        self.assertIsInstance(instance, Review)
+        self.assertTrue(issubclass(type(instance), BaseModel))
+
+        attr = ['id', 'created_at', 'updated_at', 'place_id', 'user_id', 'text']
+        for attr_name in attr:
+            self.assertTrue(hasattr(instance, attr_name))
+            self.assertIsInstance(getattr(instance, attr_name), self.attr_types[attr_name])
+
+    def test_instantiation(self):
+        """Test instantiation of Review class."""
+        self.assertEqual(str(type(self.new)), "<class 'models.review.Review'>")
+        self.assertIsInstance(self.new, Review)
+        self.assertTrue(issubclass(type(self.new), BaseModel))
+        self.assertAttributes(self.new)
 
 
 if __name__ == '__main__':
