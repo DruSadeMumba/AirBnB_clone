3af4ea20a3242e58e526d356fd1f43091ca3ce4b
Celse13 <rugiracelse13@gmail.com>
2023-10-14 02:41:00 -0700

    Add FieldNotFoundError class in  custom_exceptions.py

--- models/engine/custom_exceptions.py
+++ models/engine/custom_exceptions.py
@@ -16,3 +16,11 @@ class InstanceIdNotFoundError(Exception):
     def __init__(self, instance_id, class_name):
         self.message = f"'{class_name}' with '{instance_id}' is not found."
         super().__init__(self.message)
+
+
+class FieldNotFoundError(Exception):
+    """Handling the issue of file which if not found."""
+
+    def __init__(self, attr_name, class_name):
+        self.message = f"'{attr_name}' of '{class_name}' is not found."
+        super().__init__(self.message)
