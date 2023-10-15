4ae26101b387b5f45c5f6174d0c369ec6238c931
Dru Mumba <drusademumba@gmail.com>
2023-10-15 23:39:02 +0300

    Add tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,9 +2,6 @@
 """Unittest module for the BaseModel Class."""
 import os
 import unittest
-import uuid
-from datetime import datetime
-
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
@@ -49,45 +46,12 @@ class TestBaseModel(unittest.TestCase):
         assert_raises_type_error(self, self.base.save)
         assert_raises_type_error(self, self.base.save, "a")
 
-    def test_to_dicts(self):
-        """Tests the public instance method to_dict()."""
-        self.base.name = "Cat"
-        self.base.age = 3
-        dic = self.base.to_dict()
-        self.assertEqual(dic["id"], self.base.id)
-        self.assertEqual(dic["__class__"], type(self.base).__name__)
-        self.assertEqual(dic["created_at"], self.base.created_at.isoformat())
-        self.assertEqual(dic["updated_at"], self.base.updated_at.isoformat())
-        self.assertEqual(dic["name"], self.base.name)
-        self.assertEqual(dic["age"], self.base.age)
-
-    def test_to_dicts_args(self):
-        """Tests to_dict() with no arguments."""
-        self.tearDown()
-        assert_raises_type_error(self, self.base.to_dict())
-        assert_raises_type_error(self, self.base.to_dict, "a")
-
-    def test_to_dicts_instantiation(self):
-        """Tests instantiation with **kwargs."""
-        my_model = BaseModel()
-        my_model.name = "Abc"
-        my_model.my_number = 5
-        my_model_json = my_model.to_dict()
-        my_new_model = BaseModel(**my_model_json)
-        self.assertEqual(my_new_model.to_dict(), my_model.to_dict())
-
-    def test_instantiation_dict(self):
-        """Tests instantiation with **kwargs from custom dict."""
-        dic = {"__class__": "BaseModel",
-               "updated_at":
-                   datetime(2000, 11, 3, 20, 59, 59, 123456).isoformat(),
-               "created_at": datetime.now().isoformat(),
-               "id": uuid.uuid4(),
-               "var": "blah",
-               "int": 4,
-               "float": 3.14}
-        obj = BaseModel(**dic)
-        self.assertEqual(obj.to_dict(), dic)
+    def test_to_dict(self):
+        """Test to dic func"""
+        self.assertTrue(dict, type(self.base.to_dict()))
+        [self.assertIn(obj, self.base.to_dict())
+         for obj in self.vals if self.subTest(obj=obj)]
+        assert_raises_type_error(self, self.base.to_dict)
 
 
 if __name__ == '__main__':
